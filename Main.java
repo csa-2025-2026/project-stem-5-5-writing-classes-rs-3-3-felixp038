c72c402be6a8805691c4e8a15669a151c4d2abe9
felixp038 <phanfelix1@gmail.com>
2025-11-14 19:20:51 +0000

    working

--- Main.java
+++ Main.java
@@ -5,5 +5,8 @@ public class Main
   public static void main(String[] args)
   {
     // Create objects and test your classes in here
+    Person p1 = new Person("Willam", "Ranger", 17, "123-21-3123");
+    System.out.println(p1);
+    System.out.println(p1.getMarried);
   }
 }
--- Person.java
+++ Person.java
@@ -1,4 +1,55 @@
 public class Person
 {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String SSN;
 
+    public Person(String iFirst, String iLast, int iAge, String iSSN)
+    {
+        firstName = iFirst;
+        lastName = iLast;
+        age = iAge;
+        SSN = iSSN;
+    }
+
+    public String getFirst()
+    {
+        return firstName;
+    }
+
+    public String getLast()
+    {
+        return lastName;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public String getSSN()
+    {
+        return SSN;
+    }
+
+    public void growUp()
+    {
+        age++;
+    }
+
+    public void getMarried(String upLast)
+    {
+        lastName = upLast;
+    }
+
+    public void identityFraud(String upFirst)
+    {
+        firstName = upFirst;
+    }
+
+    public String toString()
+    {
+        return "Name: " + firstName + " " + lastName + "\n\tSSN: " + SSN + "\n\tAge: " + age;
+    }
 }
